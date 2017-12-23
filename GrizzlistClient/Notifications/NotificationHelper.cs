e33d031c705b0162ece80bb548d083e65a0a44fb
MichalcikFilip <cikomail@gmail.com>
2017-12-23 11:08:22 +0100

    Added notification helper to client

---
+++ GrizzlistClient/Notifications/NotificationHelper.cs
@@ -0,0 +1,17 @@
+﻿using Grizzlist.Client.Persistent;
+using Grizzlist.Notifications;
+using Grizzlist.Persistent;
+
+namespace Grizzlist.Client.Notifications
+{
+    static class NotificationHelper
+    {
+        public static void Notify(NotificationType type, params string[] parameters)
+        {
+            Notification notification = NotificationManager.Instance.Notify(type, parameters);
+
+            using (IRepository<Notification, long> repository = PersistentFactory.GetContext().GetRepository<Notification, long>())
+                repository.Add(notification);
+        }
+    }
+}
