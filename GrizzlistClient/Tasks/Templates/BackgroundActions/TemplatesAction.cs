5b3c9b62c51a6bb1c23cf4f0b46c7dd30c742f1e
MichalcikFilip <cikomail@gmail.com>
2018-01-02 22:39:37 +0100

    templates background action

--- GrizzlistClient/Tasks/Templates/BackgroundActions/TemplatesAction.cs
+++ GrizzlistClient/Tasks/Templates/BackgroundActions/TemplatesAction.cs
@@ -1,5 +1,11 @@
 ﻿using Grizzlist.Client.BackgroundActions;
+using Grizzlist.Client.Persistent;
+using Grizzlist.Client.UserSettings;
+using Grizzlist.Extensions;
+using Grizzlist.Logger;
+using Grizzlist.Persistent;
 using Grizzlist.Tasks;
+using Grizzlist.Tasks.Templates;
 using System;
 
 namespace Grizzlist.Client.Tasks.Templates.BackgroundActions
@@ -10,11 +16,55 @@ namespace Grizzlist.Client.Tasks.Templates.BackgroundActions
 
         public TemplatesAction()
             : base(3600)
-        { }
+        {
+            DateTime lastRun = DateTime.Today, today = DateTime.Today;
+
+            using (IRepository<Settings, long> repository = PersistentFactory.GetContext().GetRepository<Settings, long>())
+            {
+                Settings settings = SettingsFactory.GetSettings(repository);
+                lastRun = settings.LastRun.GetDate();
+            }
+
+            if (lastRun < today)
+            {
+                using (IRepository<Template, long> repository = PersistentFactory.GetContext().GetRepository<Template, long>())
+                {
+                    while (lastRun < today)
+                    {
+                        foreach (Template template in repository.GetAll())
+                        {
+                            if (template.SatisfiesCondition(lastRun))
+                            {
+                                Task newTask = template.CreateTask(lastRun);
+                                TaskCreated?.Invoke(newTask);
+
+                                repository.Update(template);
+                                Log.Info($"Task {newTask.Name} was created from template for day {lastRun.ToShortDateString()}", this);
+                            }
+                        }
+
+                        lastRun = lastRun.AddDays(1);
+                    }
+                }
+            }
+        }
 
         protected override void RunAction()
         {
+            using (IRepository<Template, long> repository = PersistentFactory.GetContext().GetRepository<Template, long>())
+            {
+                foreach (Template template in repository.GetAll())
+                {
+                    if (template.SatisfiesCondition())
+                    {
+                        Task newTask = template.CreateTask();
+                        TaskCreated?.Invoke(newTask);
 
+                        repository.Update(template);
+                        Log.Info($"Task {newTask.Name} was created from template", this);
+                    }
+                }
+            }
         }
     }
 }
