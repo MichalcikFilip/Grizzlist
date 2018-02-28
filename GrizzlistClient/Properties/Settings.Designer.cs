1e74faf71cb7b80d16837e50ea60dfdc423a2519
MichalcikFilip <cikomail@gmail.com>
2018-02-28 14:37:55 +0100

    Changed logging directory

--- GrizzlistClient/Properties/Settings.Designer.cs
+++ GrizzlistClient/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Grizzlist.Client.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Grizzlist.Client.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("log.txt")]
+        [global::System.Configuration.DefaultSettingValueAttribute("grizzlist_log.txt")]
         public string LogFile {
             get {
                 return ((string)(this["LogFile"]));
@@ -34,7 +34,7 @@ namespace Grizzlist.Client.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Logs")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\temp")]
         public string LogDirectory {
             get {
                 return ((string)(this["LogDirectory"]));
