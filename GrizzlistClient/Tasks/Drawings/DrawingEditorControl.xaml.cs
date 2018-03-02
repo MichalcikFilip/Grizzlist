b072f5e5e81ac11aaeb7bf96241aa2e0857943e6
MichalcikFilip <cikomail@gmail.com>
2018-03-02 15:42:48 +0100

    Added drawing editor

---
+++ GrizzlistClient/Tasks/Drawings/DrawingEditorControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Grizzlist.Client.Validators;
+using Grizzlist.Client.Validators.BasicValidators;
+
+namespace Grizzlist.Client.Tasks.Drawings
+{
+    /// <summary>
+    /// Interaction logic for DrawingEditorControl.xaml
+    /// </summary>
+    public partial class DrawingEditorControl : ValidatableControl
+    {
+        public DrawingEditorControl()
+        {
+            InitializeComponent();
+
+            AddValidator(new EmptyStringValidator(tbName));
+        }
+    }
+}
